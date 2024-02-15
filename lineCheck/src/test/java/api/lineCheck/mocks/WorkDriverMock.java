bd1a6ca3a743ccf0fc295e0ccffd24e84a421f96
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-15 19:51:22 -0300

    test: JPAWork repository

---
+++ lineCheck/src/test/java/api/lineCheck/mocks/WorkDriverMock.java
@@ -0,0 +1,49 @@
+package api.lineCheck.mocks;
+
+import api.lineCheck.domain.week.DaysOfTheWeek;
+import api.lineCheck.domain.work.WorkDriver;
+import com.github.javafaker.Faker;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class WorkDriverMock {
+    public UUID id;
+    public String accountName;
+    public Time startJourneyModel;
+    public Time startLineModel;
+    public Time endLineModel;
+    public String service;
+    public String logistic;
+    public String manufacture;
+    public String vehicle;
+    public List<DaysOfTheWeek> daysOfTheWeek;
+    public WorkDriverMock() {
+        Faker faker = new Faker();
+        LocalTime localTime = faker.date().past(1, TimeUnit.DAYS).toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalTime();
+        this.id = UUID.randomUUID();
+        this.accountName = faker.name().firstName();
+        this.startJourneyModel = Time.valueOf(localTime);
+        this.startLineModel = Time.valueOf(localTime);
+        this.endLineModel = Time.valueOf(localTime);
+        this.service = faker.harryPotter().spell();
+        this.logistic = faker.lordOfTheRings().location();
+        this.manufacture = faker.company().name();
+        this.vehicle = faker.aviation().airport();
+        this.daysOfTheWeek = new ArrayList<>();
+        this.daysOfTheWeek.add(faker.options().option(DaysOfTheWeek.class));
+    }
+
+    public WorkDriver main() {
+        return new WorkDriver(
+                id, accountName, startJourneyModel, startLineModel, endLineModel,
+                service, logistic, manufacture, vehicle, daysOfTheWeek
+        );
+    }
+}
