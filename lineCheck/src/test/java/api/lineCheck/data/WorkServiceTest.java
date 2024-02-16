5cfeb1912ee7409b9a7c33f4bc86b5cdc4d689d3
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-16 09:26:26 -0300

    test: work manager in work service

--- lineCheck/src/test/java/api/lineCheck/data/WorkServiceTest.java
+++ lineCheck/src/test/java/api/lineCheck/data/WorkServiceTest.java
@@ -2,11 +2,12 @@ package api.lineCheck.data;
 
 import api.lineCheck.data.usecase.WorkService;
 import api.lineCheck.domain.work.WorkDriver;
+import api.lineCheck.domain.work.WorkManager;
 import api.lineCheck.infra.repositories.JPAWork;
 import api.lineCheck.mocks.WorkDriverDbMock;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
+import api.lineCheck.mocks.WorkManagerDbMock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,7 +15,6 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,17 +23,26 @@ public class WorkServiceTest {
     public WorkService sut;
     @Mock
     public JPAWork repository;
-    public List<Object[]> dbMock = WorkDriverDbMock.main();
+    public List<Object[]> dbDriverMock = WorkDriverDbMock.main();
+    public List<Object[]> dbManagerMock = WorkManagerDbMock.main();
     @Test
-    public void should_call_repository () {
+    public void should_call_repositories() {
         sut.listWorks();
         verify(repository, times(1)).list();
+        verify(repository, times(1)).listManager();
     }
     @Test
-    public void should_return_list_of_WorkDriver () {
-        when(repository.list()).thenReturn(dbMock);
+    public void should_return_list_of_WorkDriver() {
+        when(repository.list()).thenReturn(dbDriverMock);
         List<WorkDriver> response = sut.listWorks();
-        assertEquals(response.get(0).getId(), dbMock.get(0)[0]);
-        assertEquals(response.get(0).getAccountName(), dbMock.get(0)[1]);
+        assertEquals(response.get(0).getId(), dbDriverMock.get(0)[0]);
+        assertEquals(response.get(0).getAccountName(), dbDriverMock.get(0)[1]);
+    }
+    @Test
+    public void should_return_list_of_WorkManager() {
+        when(repository.listManager()).thenReturn(dbManagerMock);
+        List<WorkManager> response = sut.listManagerWorks();
+        assertEquals(response.get(0).getId(), dbManagerMock.get(0)[0]);
+        assertEquals(response.get(0).getStartJourneyReal(), dbManagerMock.get(0)[3]);
     }
 }
