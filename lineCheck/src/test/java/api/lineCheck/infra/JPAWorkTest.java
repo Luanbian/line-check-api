8d4dac9f1538b4fdf14385dd061711f23477dd00
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-16 09:20:47 -0300

    test: infra work manager data from db

--- lineCheck/src/test/java/api/lineCheck/infra/JPAWorkTest.java
+++ lineCheck/src/test/java/api/lineCheck/infra/JPAWorkTest.java
@@ -5,6 +5,7 @@ import api.lineCheck.infra.repositories.JPAWork;
 import static org.junit.jupiter.api.Assertions.*;
 
 import api.lineCheck.mocks.WorkDriverDbMock;
+import api.lineCheck.mocks.WorkManagerDbMock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +13,6 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,11 +21,18 @@ public class JPAWorkTest {
     public JPAWork sut;
     @Mock
     public AccountJPArepositories repository;
-    public List<Object[]> dbMock = WorkDriverDbMock.main();
+    public List<Object[]> dbDriverMock = WorkDriverDbMock.main();
+    public List<Object[]> dbManagerMock = WorkManagerDbMock.main();
     @Test
-    public void should_return_list_of_object_driver_data () {
-        when(repository.findDriverWorkData()).thenReturn(dbMock);
+    public void should_return_list_of_object_driver_data() {
+        when(repository.findDriverWorkData()).thenReturn(dbDriverMock);
         List<Object[]> response = sut.list();
-        assertEquals(response, dbMock);
+        assertEquals(response, dbDriverMock);
+    }
+    @Test
+    public void should_return_list_of_object_manager_data() {
+        when(repository.findManagerWorkData()).thenReturn(dbManagerMock);
+        List<Object[]> response = sut.listManager();
+        assertEquals(response, dbManagerMock);
     }
 }
---
+++ lineCheck/src/test/java/api/lineCheck/mocks/WorkManagerDbMock.java
@@ -0,0 +1,41 @@
+package api.lineCheck.mocks;
+
+import api.lineCheck.domain.week.DaysOfTheWeek;
+import com.github.javafaker.Faker;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class WorkManagerDbMock {
+    public static List<Object[]> main() {
+        Faker faker = new Faker();
+        LocalTime localTime = faker.date().past(1, TimeUnit.DAYS).toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalTime();
+        List<Object[]> data = new ArrayList<>();
+        List<DaysOfTheWeek> daysOfTheWeeks = new ArrayList<>();
+        daysOfTheWeeks.add(faker.options().option(DaysOfTheWeek.class));
+        Object[] item = new Object[]{
+                UUID.randomUUID(),
+                "Ismael",
+                Time.valueOf(localTime),
+                Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), localTime)),
+                Time.valueOf(localTime),
+                Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), localTime)),
+                Time.valueOf(localTime),
+                Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), localTime)),
+                "Entrada 2x2",
+                "Vl.Angélica - Nova Sorocaba",
+                "Bericap",
+                "VAN 224",
+                daysOfTheWeeks
+        };
+        data.add(item);
+        return data;
+    }
+}
