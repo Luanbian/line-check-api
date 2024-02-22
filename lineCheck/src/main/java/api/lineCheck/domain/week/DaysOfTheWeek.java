411d577249d5e152302e99e7ac5ddf2cba85dd1a
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-22 16:39:02 -0300

    style: comment to know days of the week

--- lineCheck/src/main/java/api/lineCheck/domain/week/DaysOfTheWeek.java
+++ lineCheck/src/main/java/api/lineCheck/domain/week/DaysOfTheWeek.java
@@ -1,11 +1,11 @@
 package api.lineCheck.domain.week;
 
 public enum DaysOfTheWeek {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+    SUNDAY, // 0 DOM
+    MONDAY, // 1 SEG
+    TUESDAY, // 2 TER
+    WEDNESDAY, // 3 QUA
+    THURSDAY, // 4 QUI
+    FRIDAY, // 5 SEX
+    SATURDAY //6 SAB
 }
