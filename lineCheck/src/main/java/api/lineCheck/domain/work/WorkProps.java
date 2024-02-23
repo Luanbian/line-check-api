e4ba2a3f75a385c7b1933bf3ff237fe92bca45f9
Luan Bianchini <luanbianchine1@gmail.com>
2024-02-23 12:38:10 -0300

    feat: add workProps

---
+++ lineCheck/src/main/java/api/lineCheck/domain/work/WorkProps.java
@@ -0,0 +1,19 @@
+package api.lineCheck.domain.work;
+
+import api.lineCheck.domain.week.DaysOfTheWeek;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.UUID;
+
+public record WorkProps(
+        UUID accountId,
+        UUID serviceId,
+        List<DaysOfTheWeek> daysOfTheWeeks,
+        Time startJourneyModel,
+        Time startLineModel,
+        Time endLineModel,
+        UUID logistic,
+        UUID vehicle,
+        UUID manufacture
+) {}
